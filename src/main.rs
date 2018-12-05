1920f6b73e83181d535d8f5ab89604dbb88d4c0a
Kirill Taran <kirill.t256@gmail.com>
2018-12-06 00:41:25 +0300

    Square wireframe

--- src/main.rs
+++ src/main.rs
@@ -20,32 +20,46 @@ mod state;
 
 use core::*;
 use motion::{Animated, Rotation};
-use model::wireframe;
+use model::wireframe::{self, Joint};
 use model::combine;
 use model::figures;
 
 use state::*;
 
 fn main() {
-    let pyramid = figures::tetrahedron(unit_x(), unit_y(), unit_z(), unit_xyz(), blue(), red());
+    let pyramid = figures::tetrahedron(unit_x(), unit_y(), unit_z(), unit_xyz(), blue(), red())
+        .scale_eq(0.8)
+        .shift_x(0.1)
+        .shift_y(0.1)
+        .shift_z(0.1);
     let pyramid = Rotation::d3(pyramid, vec![PI / 2., PI / 3., PI / 5.], 60);
 
     let frustum = combine::tower(
             figures::triangle(unit_x(), unit_y(), unit_z(), red()),
             figures::triangle(unit_x() * 3., unit_y() * 3., unit_z() * 3., green()))
-        .scale_eq(0.3)
+        .scale_eq(0.2)
         .shift_y(2.0)
-        .shift_z(1.0);
+        .shift_z(1.0)
+        .shift_z(0.5);
 
-    let objects = Rotation::d3(motion::compose(pyramid, frustum), vec![PI / 2., PI / 3., PI / 5.], 60);
+    let objects = Rotation::d3(motion::compose(pyramid, frustum),
+                               vec![PI / 2., PI / 3., PI / 5.], 60);
 
-    let axis = combine::merge_group(vec![
-        wireframe::stick_x(0.01, 32.0, (black(), red()), 6),
-        wireframe::stick_y(0.01, 32.0, (black(), blue()), 6),
-        wireframe::stick_z(0.01, 32.0, (black(), green()), 6),
+    let cage = combine::merge_group(vec![
+        wireframe::square_xy(0.07, 3.3, (blue(), green(), red(), yellow()), 6, Joint::smooth(0.15)),
+        wireframe::square_yz(0.07, 3.3, (blue(), green(), red(), yellow()), 6, Joint::smooth(0.15)),
+        wireframe::square_zx(0.07, 3.3, (blue(), green(), red(), yellow()), 6, Joint::smooth(0.15))
     ]);
 
-    let scene = motion::compose(axis, objects);
+    let axis = combine::merge_group(vec![
+        wireframe::stick_x(0.01, 32., (black(), red()), 6),
+        wireframe::stick_y(0.01, 32., (black(), blue()), 6),
+        wireframe::stick_z(0.01, 32., (black(), green()), 6),
+    ]);
+
+    let scene = motion::compose(
+        combine::merge(axis, cage),
+        objects);
 
     {
         //debug
--- src/model/wireframe.rs
+++ src/model/wireframe.rs
@@ -5,6 +5,27 @@ use model::figures;
 use model::transform::*;
 use model::Snapshot;
 
+pub struct Joint {
+    radius: CoordFloat,
+    detailing: u16
+}
+
+impl Joint {
+    pub fn none() -> Option<Self> {
+        None
+    }
+
+    pub fn rough(radius: CoordFloat) -> Option<Self> {
+        Some(Joint { radius: radius, detailing: 6 })
+    }
+    pub fn normal(radius: CoordFloat) -> Option<Self> {
+        Some(Joint { radius: radius, detailing: 12 })
+    }
+    pub fn smooth(radius: CoordFloat) -> Option<Self> {
+        Some(Joint { radius: radius, detailing: 24 })
+    }
+}
+
 pub fn stick_x(gauge: CoordFloat, length: CoordFloat, colors: (Color, Color), detailing: u16) -> Snapshot {
     stick_z(gauge, length, colors, detailing).turn_around_y(PI / 2.)
 }
@@ -16,6 +37,42 @@ pub fn stick_y(gauge: CoordFloat, length: CoordFloat, colors: (Color, Color), de
 pub fn stick_z(gauge: CoordFloat, length: CoordFloat, colors: (Color, Color), detailing: u16) -> Snapshot {
     let offset = length / 2.;
     combine::tower(
-        figures::circle_xy(origin(), gauge, colors.1, detailing).shift_z(- offset),
+        figures::circle_xy(origin(), gauge, colors.1, detailing).shift_z(-offset),
         figures::circle_xy(origin(), gauge, colors.0, detailing).shift_z(offset))
+}
+
+pub fn square_xy(gauge: CoordFloat, side: CoordFloat, colors: (Color, Color, Color, Color),
+                 detailing: u16, balls_radius: Option<Joint>) -> Snapshot {
+    let left = stick_y(gauge, side, (colors.3, colors.0), detailing);
+    let right = stick_y(gauge, side, (colors.2, colors.1), detailing);
+    let top = stick_x(gauge, side, (colors.0, colors.1), detailing);
+    let bottom = stick_x(gauge, side, (colors.3, colors.2), detailing);
+
+    let offset = side / 2.;
+
+    let mut result = balls_radius
+        .map(|joint| vec![
+            figures::sphere_xyz(Point::new(0., -offset,  offset, 0.), joint.radius, colors.0, joint.detailing),
+            figures::sphere_xyz(Point::new(0.,  offset,  offset, 0.), joint.radius, colors.1, joint.detailing),
+            figures::sphere_xyz(Point::new(0.,  offset, -offset, 0.), joint.radius, colors.2, joint.detailing),
+            figures::sphere_xyz(Point::new(0., -offset, -offset, 0.), joint.radius, colors.3, joint.detailing),
+        ])
+        .unwrap_or(vec![]);
+
+    result.push(left.shift_x(-offset));
+    result.push(right.shift_x(offset));
+    result.push(top.shift_y(offset));
+    result.push(bottom.shift_y(-offset));
+
+    combine::merge_group(result)
+}
+
+pub fn square_yz(gauge: CoordFloat, side: CoordFloat, colors: (Color, Color, Color, Color),
+                 detailing: u16, balls_radius: Option<Joint>) -> Snapshot {
+    square_xy(gauge, side, colors, detailing, balls_radius).turn_around_y(PI / 2.)
+}
+
+pub fn square_zx(gauge: CoordFloat, side: CoordFloat, colors: (Color, Color, Color, Color),
+                 detailing: u16, balls_radius: Option<Joint>) -> Snapshot {
+    square_xy(gauge, side, colors, detailing, balls_radius).turn_around_x(PI / 2.)
 }
